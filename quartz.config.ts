51b64140477d11b8dd0e50543e401409e227980b
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2024-02-23 21:07:53 +0100

    docs: update plugin documentation (#888)
    * docs: first few plugins documented
    
    * docs: move plugin info
    
    * docs: move plugin docs to tag based system
    
    * docs: update latex example code snippet
    
    * docs: fix spelling of latex in title
    
    * docs: add missing linebreak
    
    * docs: remove plugin tag from feature pages
    
    * docs: shorten titles
    
    * docs: refine wording
    
    * docs: move plugin details for frontmatter
    
    * docs: add features/* tags
    
    * docs: update latex example
    
    * docs: make references more explicit
    
    * docs: add stubs for the remaining plugins
    
    * docs: more descriptions
    
    * docs: fix feature tags
    
    * docs: descriptions
    
    * docs: new plugin pages
    
    * docs: update configuration page
    
    * docs: more plugin work
    
    * docs: run prettier
    
    * docs: remove comments in config file and add link to docs
    
    * docs: minor fixes
    
    * docs: run prettier
    
    * docs: spelling
    
    * docs: update docs/plugins/AliasRedirects.md
    
    Co-authored-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>
    
    * docs: update docs/plugins/Assets.md
    
    Co-authored-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>
    
    * docs: update docs/plugins/CNAME.md
    
    Co-authored-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>
    
    * docs: update docs/plugins/Static.md
    
    Co-authored-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>
    
    * docs: update docs
    
    * docs: update docs/features/Mermaid diagrams.md
    
    Co-authored-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>
    
    * docs: update docs/plugins/RemoveDrafts.md
    
    Co-authored-by: Jacky Zhao <j.zhao2k19@gmail.com>
    
    * docs: update docs/plugins/Assets.md
    
    Co-authored-by: Jacky Zhao <j.zhao2k19@gmail.com>
    
    * docs: update docs/configuration.md
    
    Co-authored-by: Jacky Zhao <j.zhao2k19@gmail.com>
    
    * docs: update docs/configuration.md
    
    Co-authored-by: Jacky Zhao <j.zhao2k19@gmail.com>
    
    * docs: update docs/configuration.md
    
    Co-authored-by: Jacky Zhao <j.zhao2k19@gmail.com>
    
    * docs: some updates
    
    * docs: work in review comments
    
    ---------
    
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>
    Co-authored-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>
    Co-authored-by: Jacky Zhao <j.zhao2k19@gmail.com>

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,6 +1,11 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
+/**
+ * Quartz 4.0 Configuration
+ *
+ * See https://quartz.jzhao.xyz/configuration for more information.
+ */
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Fish Touching🐟🎣",
@@ -50,13 +55,10 @@ const config: QuartzConfig = {
       }),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting({
-        // uses themes bundled with Shikiji, see https://shikiji.netlify.app/themes
         theme: {
           light: "github-light",
           dark: "github-dark",
         },
-        // set this to 'true' to use the background color of the Shikiji theme
-        // if set to 'false', will use Quartz theme colors for background
         keepBackground: false,
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false, mermaid: false }),
