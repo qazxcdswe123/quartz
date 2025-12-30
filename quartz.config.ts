ddee2282e48daab8bf14a88dd6157d8a6a2ce950
chenyuning.neil <chenyuning.neil@bytedance.com>
2025-12-30 20:31:41 +0800

    update pageTitle

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Fish Touching🐟🎣",
+    pageTitle: "YuNing's Thought",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
