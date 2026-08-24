5eb86b429ea4b3d1d4b8b4f286e2125c6049cf9b
chenyuning.neil <chenyuning.neil@bytedance.com>
2026-08-24 17:42:02 +0800

    add AI to ignore

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: null,
     locale: "en-US",
     baseUrl: "blog.ynchen.me",
-    ignorePatterns: ["Ignore", "Secret", ".obsidian", "LLM-Wiki"],
+    ignorePatterns: ["Ignore", "Secret", ".obsidian", "LLM-Wiki", "AI"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
